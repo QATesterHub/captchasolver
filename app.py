e4178b572e08dffa712b4a6e1291be3be10b646c
Nitin <nyadav1500@gmail.com>
2025-04-09 18:59:51 +0530

    new1

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def run_bot(excel_path):
     return result_path
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port)
