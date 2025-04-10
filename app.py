d6765641f07e3ed081077782be8a015b441e227a
Nitin <nyadav1500@gmail.com>
2025-04-10 14:57:31 +0530

    app

--- app.py
+++ app.py
@@ -97,5 +97,5 @@ def download_file(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port)
